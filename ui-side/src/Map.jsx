91763f2aa77e19d1c09a538436e3fc9bb8429b2c
Nagyvee <nigelmafuta1@gmail.com>
2024-06-06 20:55:10 +0200

    save

--- ui-side/src/Map.jsx
+++ ui-side/src/Map.jsx
@@ -1,19 +1,24 @@
 import { useEffect } from "react";
-import tt from "@tomtom-international/web-sdk-maps"
+import tt from "@tomtom-international/web-sdk-maps";
 
-const Map = () =>{
-    useEffect(()=>{
-      const map = tt.map ({
-        key: "",
-        container: 'map',
-        center: [0, 0],
-        zoom:14,
-        style: 'tomtom://vectoir/1/basic'
-      });
-      return () => map.remove();
-    }, []);
+const Map = () => {
+  useEffect(() => {
+    const map = tt.map({
+      key: "8QUXut0Wm1k4FAdTrmPxLjWw3uK0oKOn",
+      container: "map",
+      center: [0, 0],
+      zoom: 14,
+      style: {
+        map: "2/basic_street-light-driving",
+        poi: "2/poi_light",
+        trafficIncidents: "2/incidents_light",
+        trafficFlow: "2/flow_relative-light",
+      },
+    });
+    return () => map.remove();
+  }, []);
 
-    return <div id="map" style={{width:'100vw' ,height: '75vh'}}/>
-}
+  return <div id="map" style={{ width: "100vw", height: "75vh" }} />;
+};
 
 export default Map;
